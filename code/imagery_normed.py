c0d13d477dc407b5327993eb7f33b391f1d2ee72
kllewers <kristen.lewers@gmail.com>
2024-04-28 15:05:04 -0600

    WIP

--- code/imagery_normed.py
+++ code/imagery_normed.py
@@ -1,5 +1,13 @@
 import cv2
 image = cv2.imread('/Users/kitlewers/Desktop/drone_project_adler/images/DJI_0862.JPG')
 
+# Convert it to grayscale
 gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+# Display the grayscale image
+cv2.imshow('Grayscale Image', gray_image)
+
+# Wait for a key press to close the window
+cv2.waitKey(0)
+
 
